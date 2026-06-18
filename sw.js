45bd06a7534b8df23e6f90b94e0e855d6c3cf6b1
gbastian22 <g.bastiann22@gmail.com>
2026-06-18 13:19:21 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'gastos-v1';
+const ASSETS = ['/'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/')))
+  );
+});
